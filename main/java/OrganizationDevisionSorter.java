70f77fe09d067a3f5e0c33d5f7e43a654ced1ae2
Юрий <mironoff2007@rambler.ru>
2019-02-20 18:05:46 +0300

    OrganizationDivisionSorter
    AddAllDepartments method implemented

--- main/java/OrganizationDevisionSorter.java
+++ main/java/OrganizationDevisionSorter.java
@@ -1,3 +1,4 @@
+import java.util.Collections;
 import java.util.TreeSet;
 
 class OrganizationDivisionSorter {
@@ -6,15 +7,32 @@ class OrganizationDivisionSorter {
     private TreeSet<String> divisions= new TreeSet<>();
 
     public OrganizationDivisionSorter(String [] arr){
-        getMainDepartments(arr);
+        AddAllDepartments(arr);
     }
 
     private void getMainDepartments(String[] arr){
         for (String s:arr)
             if (s.contains("\\")) divisions.add(s.split("\\\\")[0]);
             else divisions.add(s);
-        System.out.println();
+
     }
+    private void AddAllDepartments(String[] arr){
+
+        for (String s:arr)
+            if (s.contains("\\")) {
+                divisions.add(s);
+
+                int ind;
+                while ((ind=s.lastIndexOf("\\"))>0){
+                    s=s.substring(0,ind);
+                    divisions.add(s);
+                }
+            }
+
+            else divisions.add(s);
+
+    }
+
 
     public void printAll(){
         divisions.forEach(System.out::println);
